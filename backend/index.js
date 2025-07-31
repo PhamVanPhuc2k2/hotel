4b917e298a8649daf353af1ad1ac9c108f08ec6e
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-07-31 18:18:51 +0700

    connect database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Kết nối database thành công!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 const app = express();
 app.listen(3000, () => {
