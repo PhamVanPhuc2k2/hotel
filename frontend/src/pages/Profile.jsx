26aa6a61e55276eae7453b2bf99b2f08568535db
PhamVanPhuc2k2 <phamvanphuctnp@gmail.com>
2025-08-02 22:40:39 +0700

    Profile page UI

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const currentUser = useSelector((state) => state.auth.login.currentUser);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="Avatar"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+        />
+        <button className="p-3 bg-green-700 rounded-lg text-white uppercase cursor-pointer hover:opacity-95 disabled:opacity-85">
+          update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Accoun</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
